329e56383229000eb742cd09efd36dd2118e75ae
praveen <kumar.praveen@lenksart.in>
2019-02-11 15:57:16 +0530

    create generateMessage utility

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,7 @@
 var socket=io();
 socket.on('connect',function(){
   console.log('Connected to server');
-  socket.emit('createMessage',{
-    from: 'Kunal',
-    text: 'Dont worry!' 
-  });
+  
 });
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express=require('express');
 const socketIO=require('socket.io');
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
+const {generateMessage}=require('./utils/message');
 var app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
@@ -11,17 +12,13 @@ var io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
-  socket.emit('newMessage',{
-  from: 'Kunal',
-  text: 'See you there',
-  createdAt: 1223322
-  
-  });
+
+socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+
   socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
-  });
-  socket.on('createEmail',(newEmail)=>{
-    console.log('createEmail',newEmail);
+    io.emit('newMessage',generateMessage(message.from,message.text));
   });
 
   socket.on('disconnect',()=>{
